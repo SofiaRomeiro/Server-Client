a08c09b45e5bb60f887f29e92aba39c88a79422b
SofiaRomeiro <sofiaromeiro23@tecnico.ulisboa.pt>
2022-01-25 12:02:01 +0000

    tfs_mount

--- tecnicofs_ex2/client/tecnicofs_client_api.c
+++ tecnicofs_ex2/client/tecnicofs_client_api.c
@@ -1,42 +1,77 @@
 #include "tecnicofs_client_api.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
 
 #define PERMISSIONS 0777
 #define BUFFER_SIZE 40
 
+static int session_id = -1;
+
 int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
     /* TODO: Implement this */
     int fcli, fserv;
+    ssize_t n;
     char buffer[BUFFER_SIZE];
+    memset(buffer, '\0', sizeof(buffer));
 
-    // abre a pipe para o servidor 
-    unlink(client_pipe_path);
-
-    if (mkfifo(client_pipe_path, PERMISSIONS) < 0) {
+     // named pipe - server
+    if ((fserv = open(server_pipe_path, O_WRONLY | O_NONBLOCK)) < 0) {
+        printf("[INFO - tfs_mount] Error opening server: %s\n", strerror(errno));
         return -1;
     }
 
-    if ((fcli = open(client_pipe_path, O_RDONLY)) < 0) 
-        return -1;
-
-    if ((fserv = open(server_pipe_path, O_WRONLY)) < 0)
-        return -1;
-
-    memset(buffer, '\0', sizeof(buffer));
-    // manda lhe o op code = 1
-    memcpy(buffer, (char *)TFS_OP_CODE_MOUNT, sizeof(char));
-    // manda lhe o nome do client_pipe 
     size_t str_len = strlen(client_pipe_path);
+
+    sprintf(buffer, "%d", TFS_OP_CODE_MOUNT);
+
     memcpy(buffer + 1, client_pipe_path, str_len);
 
-    // recebe 0 => sucesso, se nao -1
+    if (write(fserv, buffer, sizeof(buffer)) == -1) {
+        printf("[tfs_mount] Error writing\n");
+        return -1;
+    }        
 
-    return -1;
+    memset(buffer, '\0', sizeof(buffer));
+
+    // abre a pipe para o servidor 
+    if (unlink(client_pipe_path) != 0 && errno != ENOENT) {
+        fprintf(stderr, "[ERR]: unlink(%s) failed: %s\n", client_pipe_path, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if (mkfifo(client_pipe_path, PERMISSIONS) != 0) { // criar named pipe - client
+        printf("[tfs_mount] mkfifo failed\n");
+        return -1;
+    }
+
+    // named pipe - client
+    if ((fcli = open(client_pipe_path, O_RDONLY)) < 0) {
+        printf("[tfs_mount] Error opening client\n");
+        return -1;
+    } 
+
+    printf("[client_api] Reading from server...\n");       
+
+    if ((n = read(fcli, buffer, sizeof(int))) == -1) {
+        printf("[tfs_mount] Error reading\n");
+        return -1;
+    } 
+
+    printf("[api_client] n is %ld\n", n);       
+
+    printf("[client_api] Finish reading!\n");
+
+    session_id = atoi(buffer); 
+
+    printf("[ + ] Session_id = %d\n", session_id);
+
+    return 0;
 }
 
 int tfs_unmount() {
--- tecnicofs_ex2/fs/tfs_server.c
+++ tecnicofs_ex2/fs/tfs_server.c
@@ -1,60 +1,168 @@
 #include "operations.h"
 #include "client/tecnicofs_client_api.h"
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
+
+#define SIZE 99
 
 #define PERMISSIONS 0777
-#define SIZE 100
-#define CLI_SIZE 40
-
+#define CLI_PIPE_SIZE 39
 #define S 20
 #define SIZE_OF_CHAR 1
 #define NAME_PIPE_SIZE 40
 
-static int open_sessions;
-
 typedef struct {
     int session_id;
     char pipe_name[40];
-    // client ??
 } session_t;
+
+typedef enum {FREE_POS = 1, TAKEN_POS = 0} session_state_t;
+
+static int open_sessions;
+static session_t sessions[S];
+static session_state_t free_sessions[S];
+
+
+int find_free_pos() {
+
+    int free = -1;
+    
+    for (int i = 0; i < S; i++) {
+        if (free_sessions[i] == FREE_POS) {            
+            free = i;
+            break;
+        }
+    }
+
+    if (free == -1) {
+        //printf("[ tfs_server ] All positions occupied\n");
+        exit(EXIT_FAILURE);
+    } 
+
+    free_sessions[free] = TAKEN_POS;
+
+    //printf("[tfs_server] free pos = %d\n", free);
+    
+    return free;
+}
+
+int tfs_handle_mount(char pipe_name[], int fserv) {
+
+    int fcli;
+    int free = 0;
+
+    memset(pipe_name, '\0', CLI_PIPE_SIZE + 1);
+
+    //printf("[tfs_handle_mount] Reading...\n");
+    
+    ssize_t n = read(fserv, pipe_name, NAME_PIPE_SIZE);
+
+    if (n == -1) {
+        //printf("[ ERROR ] Reading : Failed\n");
+        return -1;
+    }
+
+    if (open_sessions == S) {
+        //printf("[ tfs_server ] tfs_mount: Reached limit number of sessions, please wait\n");
+        return -1;
+    }
+
+    open_sessions++;
+
+    free = find_free_pos();
+
+    session_t *session = &(sessions[free]);
+
+    size_t len = strlen(pipe_name); //sem '\0'
+
+    memset(session->pipe_name, '\0', sizeof(session->pipe_name));
+
+    memcpy(session->pipe_name, pipe_name, len);
+
+    // choose session id
+    
+    session->session_id = free;
+
+    // enviar ao client o session id
+
+    if ((fcli = open(pipe_name, O_WRONLY) < 0)) {
+        //printf("[ tfs_server ] tfs_mount : Failed to open client pipe\n");
+        return -1;
+    }
+
+    char session_id_cli[sizeof(char) + 1];
+    memset(session_id_cli, '\0', sizeof(session_id_cli));
+    //sprintf(session_id_cli, "%d", free);k
+
+    session_id_cli[0] = (char) free + '0';
+
+    n = 0;
+
+    n = write(fcli, session_id_cli, sizeof(char));
+
+    printf("[tfs_server] Write to client... n is %ld\n", n);
+
+    if (n == -1) {
+        //printf("[ ERROR ] Reading : Failed\n");
+        return -1;
+    }
+
+    return 0;
+
+}
 
 int main(int argc, char **argv) {
 
-    session_t sessions[S];
-    int free_sessions[S];
-    int fserv;
+    int fserv, fcli;
     ssize_t n;
-    char command;
+    int command;
     char *server_pipe;
+    char buffer[SIZE + 1];;
+    char pipe_name[CLI_PIPE_SIZE + 1];
 
-    char buffer[SIZE];
-
-    memset(free_sessions, 1, sizeof(free_sessions));
+    for (int i = 0; i < S; i++) {
+        free_sessions[i] = FREE_POS;
+    }
 
     open_sessions = 0;
-
-    // ---------------------------------------- OPEN SERVER PIPE ---------------------------------------------
+    memset(buffer, '\0', SIZE + 1);
+    memset(pipe_name, '\0', CLI_PIPE_SIZE + 1);    
 
     if (argc < 2) {
-        printf("Please specify the pathname of the server's pipe.\n");
-        return 1;
-    }
-    else {
-        server_pipe = argv[1];
+        //printf("Please specify the pathname of the server's pipe.\n");
+        exit(EXIT_FAILURE);
     }
 
-    unlink(server_pipe);
+    server_pipe = argv[1];
 
-    if (mkfifo(server_pipe, PERMISSIONS) < 0) {
+    printf("Starting TecnicoFS server with pipe called %s\n", server_pipe);
+
+    //printf("[tfs_server] server_pipe : %s\n", server_pipe);
+
+    if (unlink(server_pipe) != 0 && errno != ENOENT) {
+        //printf(stderr, "[ERR]: unlink(%s) failed: %s\n", server_pipe, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if (mkfifo(server_pipe, PERMISSIONS) != 0) {
+        //printf("[tfs_server] mkfifo failed: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+
+    //printf("[INFO - tfs_server] Server pipe = |%s|\n", server_pipe);
+
+    
+    if ((fserv = open(server_pipe, O_RDONLY)) < 0)  {
+        //printf("[tfs_server] Open failed : %s\n", strerror(errno));
         return -1;
     }
-
-    if ((fserv = open(server_pipe, O_RDONLY)) < 0) 
-        return -1;
+        
 
     // ----------------------------------------- START RESPONDING TO REQUESTS --------------------------------------
 
@@ -64,7 +172,7 @@ int main(int argc, char **argv) {
 
         n = read(fserv, buffer, SIZE_OF_CHAR);
 
-        if (n == 0) {
+        if (n == 0) { //EOF
             close(fserv);
             if ((fserv = open(server_pipe, O_RDONLY)) < 0) 
                 return -1;
@@ -72,66 +180,31 @@ int main(int argc, char **argv) {
         }
 
         if (n == -1) {
-            printf("[ ERROR ] Reading : Failed\n");
+            //printf("[ ERROR ] Reading : Failed\n");
             return -1;
         }
 
-        command = buffer[0];      
+        command = ((int) buffer[0]) - '0';    
 
         switch(command) {
             case (TFS_OP_CODE_MOUNT):
 
-                memset(buffer, '\0', sizeof(buffer));
-                n = read(fserv, buffer, NAME_PIPE_SIZE);
+                //printf("[INFO - tfs_server] Mounting...\n");
 
-                if (n == -1) {
-                    printf("[ ERROR ] Reading : Failed\n");
+                if (tfs_handle_mount(pipe_name, fserv) == -1) {
+                    //printf("[ tfs_server ] Error while mounting\n");
                     return -1;
                 }
 
-                buffer[n] = '\0';
-
-                if (open_sessions == S) {
-                    printf("[ tfs_server ] tfs_mount: Reached limit number of sessions, please wait\n");
+                if ((fcli = open(pipe_name, O_WRONLY)) < 0) 
                     return -1;
-                }
-                open_sessions++;
-
-                // choose place for the new session
-                // choose the array position to put the new session
-
-                int free = -1;
-
-                for (int i = 0; i < S; i++) {
-                    if (free_sessions[i] == 1) {
-                        free = i;
-                        break;
-                    }
-                }
-
-                if (free == -1) return -1;
-
-                free_sessions[free] = 0;
-
-                session_t *session = &(sessions[free]);
-
-                size_t len = strlen(buffer);
-
-                memset(session->pipe_name, '\0', sizeof(session->pipe_name));
-
-                memcpy(session->pipe_name, buffer, strlen(buffer)));
-                session->pipe_name[len] = '\0';
-
-                // choose session id
-                
-                session->session_id = free;
 
             break;
 
             case (TFS_OP_CODE_UNMOUNT):
 
                 if (open_sessions == 0) {
-                    printf("[ tfs_server ] tfs_mount: There are no open sessions, please open one before unmount\n");
+                    //printf("[ tfs_server ] tfs_mount: There are no open sessions, please open one before unmount\n");
                     return -1;
                 }
                 open_sessions--;
@@ -153,16 +226,13 @@ int main(int argc, char **argv) {
             break;
 
             default:
+                //printf("[tfs_server] Switch case : No correspondance\n");
+                return -1;
             break;
 
 
         }
     }
-
-
-
-    char *pipename = argv[1];
-    printf("Starting TecnicoFS server with pipe called %s\n", pipename);
 
     /* TO DO */
 
