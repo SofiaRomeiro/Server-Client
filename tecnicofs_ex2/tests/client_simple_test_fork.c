9cd1d37a31553e268902accd7baee36132651f53
SofiaRomeiro <sofiaromeiro23@tecnico.ulisboa.pt>
2022-01-31 20:02:10 +0000

    updates

--- tecnicofs_ex2/tests/client_simple_test_fork.c
+++ tecnicofs_ex2/tests/client_simple_test_fork.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
     char *path = "/f1";
     char buffer[40];
 
-    int f, state;
+    int f, state, pid1, pid2;
     ssize_t r;
 
     if (argc < 3) {
@@ -26,7 +26,9 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    int pid1 = fork();
+    pid1 = fork();
+    pid2 = fork();
+
     if (pid1 == 0) {
         assert(tfs_mount(argv[1], argv[2]) == 0);
         exit(0);
@@ -34,44 +36,25 @@ int main(int argc, char **argv) {
         pid1 = wait(&state);
     }
 
-    int pid2 = fork();
     if (pid2 == 0) {
-        assert(tfs_mount(argv[1], argv[2]) == 0);
+        assert(tfs_mount(argv[3], argv[2]) == 0);
         exit(0);
     } else {
         pid2 = wait(&state);
     }
 
-
-    printf("Mount concluded with success!\n");
-
     f = tfs_open(path, TFS_O_CREAT);
     assert(f != -1);
-
-
     r = tfs_write(f, str, strlen(str));
     assert(r == strlen(str));
-
-
     assert(tfs_close(f) != -1);
-
-
     f = tfs_open(path, 0);
     assert(f != -1);
-
-
     r = tfs_read(f, buffer, sizeof(buffer) - 1);
-
-    //printf("r is %ld\n", r);
-
     assert(r == strlen(str));
-
     buffer[r] = '\0';
-
     assert(strcmp(buffer, str) == 0);
-
     assert(tfs_close(f) != -1);
-
     assert(tfs_unmount() == 0);
 
     printf("Successful test.\n");
